ddaae21a49ff3eabbb3b98b43a283aeaca54d2b9
Joshua Dapitan <joshuadapitan@gmail.com>
2017-07-24 02:58:27 +0800

    Initial commit

---
+++ LatestAnimu/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace LatestAnimu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvReleases = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvReleases
+            // 
+            this.lvReleases.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvReleases.Location = new System.Drawing.Point(42, 34);
+            this.lvReleases.Name = "lvReleases";
+            this.lvReleases.Size = new System.Drawing.Size(506, 388);
+            this.lvReleases.TabIndex = 0;
+            this.lvReleases.UseCompatibleStateImageBehavior = false;
+            this.lvReleases.View = System.Windows.Forms.View.Details;
+            this.lvReleases.DoubleClick += new System.EventHandler(this.lvReleases_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Latest Anime Releases";
+            this.columnHeader1.Width = 234;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 458);
+            this.Controls.Add(this.lvReleases);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvReleases;
+        public System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ LatestAnimu/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using HtmlAgilityPack;
+
+namespace LatestAnimu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = web.Load("http://www.gogoanime.to/");
+            HtmlNode[] nodes = document.DocumentNode.SelectNodes(@"//td[@class='redgr']//a").ToArray();
+            foreach (HtmlNode node in nodes)
+            {
+                String[]items={node.InnerText, node.Attributes["href"].Value};
+                lvReleases.Items.Add(new ListViewItem(items));
+            }
+            Console.ReadLine();
+        }
+
+        private void lvReleases_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(lvReleases.SelectedItems[0].SubItems[1].Text);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
